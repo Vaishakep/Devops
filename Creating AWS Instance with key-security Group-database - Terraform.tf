f35493eda7aa31c9ce6f9418e5111bc08744c440
Vaishakep <epvaishak@gmail.com>
2019-11-20 11:57:22 +0530

    Update and rename Creating AWS Instance with key-security Group-database - Terraform.txt to Creating AWS Instance with key-security Group-database - Terraform.tf

--- Creating AWS Instance with key-security Group-database - Terraform.tf
+++ Creating AWS Instance with key-security Group-database - Terraform.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    access_key = "AKIAQQU4ZUCPMMABKVJD"
-    secret_key = "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"
+    access_key = "AKIAQQU4ZxxxMMABKVJD"
+    secret_key = "ZXp1hVGKC7QqViDxxxximUqQsgrWBiUnUK45Yj8l"
     region = "us-west-2"
 }
 
 
 resource "aws_key_pair" "sshkey" {
   key_name   = "terraform-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpniPy2pe7R4g6s7KmGatuIhaq6P8FKNOFnMJRmcYx1gg3yH40Mvd+R3YT9HFLvEirYvYnT0bYvAnPabLGPwxZDkCayA/LvQe0/BxlhzVONgghGGoNLhUwSQ7SjUjJBuEmYLKhs7qp213OY8LVN78piG3Ida5ppj7Y+oxM9vDRFuQmyvv3wRJRYyU3+M4/ZwzpUr+fLNBRgB3boTTTiyXW17FIWd/T+zubVIo3I3U0hBoRFhrE3pEFCdXaq2Nc/0UmjfgygyjZTHqwgmOJPBrB4Mr1Dus8oJQyeoK1xHpKREIJSnkbSSMWoclN4LaTnn0Yj7IOvT3NjENNCUufgc6R root@MSI"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpniPy2pe7R4g6s7KmGatuIhaqxxxxxFKNOFnMJRmcYx1gg3yH40Mvd+R3YT9HFLvEirYvYnT0bYvAnPabLGPwxZDkCayA/LvQe0/BxlhzVONgghGGoNLhUwSQ7SjUjJBuEmYLKhs7qp213OY8LVN78piG3Ida5ppj7Y+oxM9vDRFuQmyvv3wRJRYyU3+M4/ZwzpUr+fLNBRgB3boTTTiyXW17FIWd/T+zubVIo3I3U0hBoRFhrE3pEFCdXaq2Nc/0UmjfgygyjZTHqwgmOJPBrB4Mr1Dus8oJQyeoK1xHpKREIJSnkbSSMWoclN4LaTnn0Yj7IOvT3NjENNCUufgc6R root@MSI"
 }
 
 resource "aws_security_group" "terraform-security" {
@@ -50,7 +50,7 @@ resource "aws_security_group" "terraform-security" {
 
 resource "aws_instance" "terraform" {
 
-    ami = "ami-0a85857bfc5345c38"
+    ami = "ami-0a8585xxbfc5345c38"
     instance_type = "t2.micro"
     key_name = "terraform-key"
     availability_zone  = "us-west-2a"
@@ -64,7 +64,7 @@ resource "aws_instance" "terraform" {
 
 resource "aws_instance" "database" {
 
-    ami = "ami-08d489468314a58df"
+    ami = "ami-08d4xxx68314a58df"
     instance_type = "t2.micro"
     key_name = "terraform-key"
     availability_zone  = "us-west-2a"
