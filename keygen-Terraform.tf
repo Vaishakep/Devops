bf0b3a30359461e7b66c8f5eacb9ab3fa71d80fb
Vaishakep <epvaishak@gmail.com>
2019-11-20 10:20:28 +0530

    Update keygen-Terraform.tf

--- keygen-Terraform.tf
+++ keygen-Terraform.tf
@@ -1,13 +1,13 @@
 Generating a keypair in AWS using IAM user
 
 provider "aws" {
-    access_key = "AKIAQQU4ZUCPMMABKVJD"
-    secret_key = "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"
+    access_key = "AKIAQQU4xxxxxPMMABKVJD"
+    secret_key = "ZXp1hVGKC7QqViDvPtxxxxxgrWBiUnUK45Yj8l"
     region = "us-west-2"
 }
 
 
 resource "aws_key_pair" "sshkey" {
   key_name   = "terraform-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpniPy2pe7R4g6s7KmGatuIhaq6P8FKNOFnMJRmcYx1gg3yH40Mvd+R3YT9HFLvEirYvYnT0bYvAnPabLGPwxZDkCayA/LvQe0/BxlhzVONgghGGoNLhUwSQ7SjUjJBuEmYLKhs7qp213OY8LVN78piG3Ida5ppj7Y+oxM9vDRFuQmyvv3wRJRYyU3+M4/ZwzpUr+fLNBRgB3boTTTiyXW17FIWd/T+zubVIo3I3U0hBoRFhrE3pEFCdXaq2Nc/0UmjfgygyjZTHqwgmOJPBrB4Mr1Dus8oJQyeoK1xHpKREIJSnkbSSMWoclN4LaTnn0Yj7IOvT3NjENNCUufgc6R root@MSI"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQxxxxxxxxxx2pe7R4g6s7KmGatuIhaq6P8FKNOFnMJRmcYx1gg3yH40Mvd+R3YT9HFLvEirYvYnT0bYvAnPabLGPwxZDkCayA/LvQe0/BxlhzVONgghGGoNLhUwSQ7SjUjJBuEmYLKhs7qp213OY8LVN78piG3Ida5ppj7Y+oxM9vDRFuQmyvv3wRJRYyU3+M4/ZwzpUr+fLNBRgB3boTTTiyXW17FIWd/T+zubVIo3I3U0hBoRFhrE3pEFCdXaq2Nc/0UmjfgygyjZTHqwgmOJPBrB4Mr1Dus8oJQyeoK1xHpKREIJSnkbSSMWoclN4LaTnn0Yj7IOvT3NjENNCUufgc6R root@MSI"
 }
